0b4590760f70492bd93ea15291fc436f6f5bb487
Konstantin Nazarenko <knazarenko@phreesia.com>
2023-07-16 22:23:21 +0200

    Add project files.

---
+++ LinksConfigurationAssignment/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using Models;
+
+namespace LinksConfigurationAssignment.Controllers
+{
+	public class HomeController : Controller
+	{
+		private readonly SocialMediaLinksOptions _socialMediaLinkOptions;
+
+		public HomeController(IOptions<SocialMediaLinksOptions> socialMediaLinkOptions) 
+		{
+			_socialMediaLinkOptions = socialMediaLinkOptions.Value;
+		}
+
+		[Route("/")]
+		public IActionResult Index()
+		{
+			ViewBag.FacebookLink = _socialMediaLinkOptions.Facebook;
+			ViewBag.YoutubeLink = _socialMediaLinkOptions.Youtube;
+			ViewBag.TwitterLink = _socialMediaLinkOptions.Twitter;
+			ViewBag.InstagramLink = _socialMediaLinkOptions.Instagram;
+			return View();
+		}
+	}
+}
---
+++ LinksConfigurationAssignment/Program.cs
@@ -0,0 +1,12 @@
+using Models;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllersWithViews();
+builder.Services.Configure<SocialMediaLinksOptions>(builder.Configuration.GetSection("SocialMediaLinks"));
+
+var app = builder.Build();
+
+app.UseStaticFiles();
+app.MapControllers();
+
+app.Run();
